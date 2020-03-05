2050b6aeb20f992c34da537faa0848f500321959
FullStackAbbs <60401214+FullStackAbbs@users.noreply.github.com>
2020-03-04 23:36:25 -0500

    answer

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 
-let datetemplate= document.querySelector('input')
-console.log(datetemplate)
+let datetemplate= document.querySelector('input');
+let buttonElement = document.querySelector('button');
+buttonElement.addEventListener('click',(function(){
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=P8MoaznCZX9CrNEjgG1rwD6geT99yYcX3CjnK9dD&date=${datetemplate.value}`)
+    // .value parse calander input date only
+      .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+      .then(response => {
+        console.log(response)
+        let mediaType = response.media_type
+        if(mediaType === "video"){
+          let videoSrc = response.url;
+          document.querySelector('iframe').src = videoSrc;
+          document.querySelector('img').style.visibility = "hidden";
+        }else if(mediaType === "image"){
+          let imgSrc= response.url;
+          document.querySelector('img').src = imgSrc;
+          document.querySelector('iframe').style.visibility = "hidden";
 
-fetch(`https://api.nasa.gov/planetary/apod?api_key=P8MoaznCZX9CrNEjgG1rwD6geT99yYcX3CjnK9dD&date=${datetemplate.value}`)
-  // .value parse calander input date only
-    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
-    .then(response => {
-      console.log(response)
-      // iframe from youtube
-      // display none
-      // loop made for if response.media_type
-      // choose the query selector
-      // width and height already stated
+        }
 
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-        alert("sorry, there are no results for your search")
-    });
+
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+          alert("sorry, there are no results for your search")
+      });
+
+}
+))

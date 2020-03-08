d6860838c103c62ecfff1e998ff46e19dab0d4cc
Sebastian Vivas <60440088+sebastian-vivas@users.noreply.github.com>
2020-03-07 23:44:01 -0500

    Add files via upload

---
+++ simpleApiNasa/js/main.js
@@ -0,0 +1,19 @@
+let nasaApi = {
+  button: document.querySelector("button"),
+  fetchPhoto: () => {
+  const input = document.querySelector('input').value;
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=tHptlbZkjhuFPLoHyWQTk4pe3Tbe3GLBTHKY8EU1&date=${input}`)
+      .then(res => res.json())
+      .then(response => {
+      document.getElementById("photo").src = response.url;
+      document.querySelector("iFrame").src = response.url;
+      })
+      .catch(err => {
+        console.log(`error ${err}`);
+        alert("sorry, there are no results for your search");
+      })
+    }
+  }
+
+
+nasaApi.button.addEventListener('click', nasaApi.fetchPhoto);

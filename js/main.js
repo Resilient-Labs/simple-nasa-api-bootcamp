4d7ce33cbd2ec6fb8b93be2628f1b6040c090144
zjacobsdev <z.jacobsdev@gmail.com>
2020-03-18 01:47:15 -0400

    added style, added date conditional

--- js/main.js
+++ js/main.js
@@ -1,56 +1,74 @@
 const key = 'WYGg8Zwt3nEabn0rT5txzuB4aopp9RqSXBUZRViI'
-let date
+let inDate
 
-document.querySelector(`#submit`).addEventListener("click", (e) =>{
- //e.preventDefault()
- getDate()
-})
+document.querySelector(`#btn-submit`).addEventListener("click", getDate)
 
 function getDate(){
- date = document.querySelector(`input`).value
- console.log(date)
 
- //add conditional for if the date is beyond the current date
+
+    let sec = document.querySelector(`#content`)
+
+  
+    if(sec.hasChildNodes() === true){ //removes all children from sec
+      
+      while (sec.firstChild) {
+        sec.removeChild(sec.lastChild);
+      }
+  
+    }
+
+ inDate= document.querySelector(`input`).value
+
+ 
+
+ const selectedDate= new Date(`${inDate}T00:00:00`)
+ var today = new Date()
+
+ if (selectedDate.getTime()> Date.now()){       // conditional for if the date is beyond the current date
+
+     alert(`Enter a date before ${today.getMonth} ${today.getDate}, ${today.getFullYear}`) //* the object is empty for some reason
+    return
+ }
+
  getData()
 
 }
+
+
 function getData(){
-fetch (`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`)
+    fetch (`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${inDate}`)
 
-.then(msg => msg.json()) 
+    .then(msg => msg.json()) 
 
-.then(response => {
-    console.log(response)
+    .then(response => {
 
-   if (response.media_type === `image`){
+        let dataImg = response.hdurl
+        let dataVideo = response.url
+        let dataDate = response.date
 
-    let dataImg = response.hdurl
-    let dataDate = response.date
-   // let dataQuote = response.explanation
-    formatResponseImg(dataImg,dataDate)
+    if (response.media_type === `image`){
+
+        formatResponseImg(dataImg,dataDate)
+
+    } else if (response.media_type ===`video`){
     
-   } else if (response.media_type ===`video`){
-        let dataVideo = response.hdurl
-        dataDate = response.date
-        // dataQuote = response.explanation
         formatResponseVideo(dataVideo,dataDate)
 
-   }
-  
-})
+    }
+    
+    })
 
+    .catch(err => {
 
-.catch(err => {
-
-    console.log(`error ${err}`)
-    alert("sorry, there are no results for your search")
-});
+        console.log(`error ${err}`)
+        alert("sorry, there are no results for your search")
+    });
 }
 
 function formatResponseImg(dataImg,dataDate){
     let imgDate = document.createElement(`span`)
     imgDate.textContent = dataDate
-    imgDate.style.color =`white`
+    imgDate.style.color =`yellow`
     let imgbg = document.createElement(`section`)
     imgbg.src = dataImg
     imgbg.classList.add(`apiImg`)
@@ -58,13 +76,15 @@ function formatResponseImg(dataImg,dataDate){
     imgbg.appendChild(imgDate)
     document.querySelector(`#content`).appendChild(imgbg);
 }
-function formatResponseVideo(dataVideo,dataDate,dataQuote){
-    let imgDate = document.createElement(`span`)
-    let img = document.createElement(`img`)
-    img.src = dataImg
-    img.classList.add(`apiImg`)
-    document.querySelector(`#content`).appendChild(img);
-
+function formatResponseVideo(dataVideo,dataDate){
+    let video = document.createElement(`iframe`)
+    video.src = dataVideo
+    video.width = '400'
+    video.height = '300'
+    video.style.border = '1px solid yellow'
+    video.style.marginLeft = '35%'
+    document.querySelector(`#content`).appendChild(video);
+ console.log("Its a video")
 
 }
 

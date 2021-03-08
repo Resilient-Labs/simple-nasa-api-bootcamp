1c6cafc4ce585b9114552f9e9a4f0748f5766cb7
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-08 01:46:22 -0500

    turn in date update

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-let url = `https://api.nasa.gov/planetary/apod?api_key=reXXQE0EZauoLQt1rzBo4JQ9cmpl2nbXvp5568wN`
+
+document.querySelector('button').addEventListener('click', spacePic)
+
+
+let api_key = "reXXQE0EZauoLQt1rzBo4JQ9cmpl2nbXvp5568wN"
+function spacePic(){
+
+    const inputVal = document.querySelector('input').value
+    const url = `https://api.nasa.gov/planetary/apod?api_key=reXXQE0EZauoLQt1rzBo4JQ9cmpl2nbXvp5568wN&date=${inputVal}`
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data)
+        let description = data.explanation
+        let photos = data.url
+        let inputValue =data.date
+        let name = data.title
+        document.querySelector('h3').innerText = description
+        document.querySelector('img').src = photos
+        document.querySelector('.name').innerText = name
+     
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+
+}

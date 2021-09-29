e733252716979b8a5e5cbebc828590473039d6f0
samekh resh <samekh.resh@gmail.com>
2021-09-29 18:14:49 -0400

    bare bones simple nasa api

---
+++ nasaSimple/js/main.js
@@ -0,0 +1,36 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+//
+
+document.querySelector('button').addEventListener('click', getImage)
+function findImgUrl(data){
+    if(data.thumbnail_url){
+        document.querySelector("img").src = data.thumbnail_url;
+    }else if(data.hdurl){
+        document.querySelector("img").src = data.hdurl
+    }
+}
+
+function getImage() {
+
+    
+  let date = document.querySelector("input").value;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=4jmpsqDyPFJsxCjgwjpeKbWBqigzT2e1PvZebEEg&date=${date}&thumbs=true`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        if(data.msg){
+            document.querySelector("h2").innerText = ''
+            document.querySelector("h3").innerText = data.msg
+        }else {
+            console.log(data);
+            findImgUrl(data)
+            document.querySelector("h2").innerText = data.title;
+            document.querySelector("h3").innerText = data.explanation;}
+    })
+
+    .catch((err) => {
+      console.log(`Error ${err}`);
+    });
+
+}

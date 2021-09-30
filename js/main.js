421012800ff22338b861e716a3bf5ac77eb25daf
Natasha Torres <natashaxtorres@gmail.com>
2021-09-29 22:15:01 -0400

    CSS updates on NASA API

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 //edge cases - dates not included, videos will pop up, 
 document.querySelector('button').addEventListener('click', getPicOfTheDay)
-// document.querySelector('iframe').style.display = 'none'
-// window.addEventListener('load', (newPage) => {
-//     document.querySelector('iframe').style.display = 'none'
-// });
 
 function getPicOfTheDay() {
 
     let val = document.querySelector('input').value
+
     const urlNasa = `https://api.nasa.gov/planetary/apod?api_key=xTNPAIEci25JC2DNB8p2AibY04nWWyonFKdCN0Nz&date=${val}`
 
     fetch(urlNasa)
@@ -19,17 +16,17 @@ function getPicOfTheDay() {
                 document.querySelector('h3').innerText = data.msg
                 document.querySelector('h2').innerText = ""
                 document.querySelector('iframe').hidden = true
-                document.querySelector('img').hidden = true
+                document.querySelector('#nasaPhoto').hidden = true
             } else {
                 if (data.media_type === "image") {
-                    document.querySelector('img').src = data.url
-                    document.querySelector('img').hidden = false
+                    document.querySelector('#nasaPhoto').src = data.url
+                    document.querySelector('#nasaPhoto').hidden = false
                     document.querySelector('iframe').hidden = true
 
                 } else if (data.media_type === "video") {
                     document.querySelector('iframe').src = data.url
                     document.querySelector('iframe').hidden = false
-                    document.querySelector('img').hidden = true
+                    document.querySelector('#nasaPhoto').hidden = true
                 }
                 document.querySelector('h3').innerText = data.explanation
                 document.querySelector('h2').innerText = data.title

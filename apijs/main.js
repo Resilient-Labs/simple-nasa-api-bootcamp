f8426883986ab41e0b03e31a7dc1ba0cf0ece8cb
Enaisha Savage <89624071+Enaisha15@users.noreply.github.com>
2021-10-02 18:02:30 -0400

    Update main.js
    Redacted Key
    // collaborated with team Hayden to complete successfully team project

--- apijs/main.js
+++ apijs/main.js
@@ -1,10 +1,12 @@
- document.querySelector('button').addEventListener("click" ,information)
+// collaborated with team Hayden to complete successfully team project 
+
+document.querySelector('button').addEventListener("click" ,information)
 
 function information(){
   let date = document.querySelector('input').value
   let image = document.querySelector('#picture')
   // let paragraph = document.querySelector('#description')
-  const url = ('https://api.nasa.gov/planetary/apod?api_key=lZCQc1FPi5m88SmbE545L98htQK0susLG5SR0GPo&date='+ date)
+  const url = ('https://api.nasa.gov/planetary/apod?api_key=()='+ date)
   
   fetch(url)
     .then(res => res.json()) 
@@ -17,4 +19,5 @@ function information(){
       alert(`error ${err}`) 
   }); 
 }
+// collaborated with team Hayden to complete successfully team project  
 

4e0ae5b86a5b7054415ef755a492748e1487dc2a
Kelly Chhe <kellykchhe@gmail.com>
2022-05-07 20:56:28 -0400

    Enter a date, displays NASA picture of the day

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 
 document.querySelector('button').addEventListener('click', getPicture)
+let image = document.querySelector('img')
+let video = document.querySelector('iframe')
 
 function getPicture(){
     let pictureDate = document.querySelector('input').value
@@ -10,11 +12,33 @@ function getPicture(){
     .then(data => {
         console.log(data.date)
         document.querySelector('h2').innerText = data.title
-        document.querySelector('img').src = data.hdurl
+        
         document.querySelector('h3').innerText = data.explanation
+        if (data.media_type === 'image'){
+            video.src = ''
+            if (image.classList.contains('hidden')){
+                image.src = data.hdurl
+                image.classList.toggle('hidden')
+                video.classList.toggle('hidden')
+            }else{
+                image.src = data.hdurl
+            }
+        } else if(data.media_type === 'video'){
+            image.src = ''
+            if (video.classList.contains('hidden')){
+                video.src = data.hdurl
+                video.classList.toggle('hidden')
+                image.classList.toggle('hidden')
+            }else{
+                video.src = data.hdurl
+            }
+        } else{
+            alert('Unsupported Media Type')
+        }
     })
     .catch(err => {
         console.log(`error ${err}`)
     })
 }
 
+//feb 3 2021

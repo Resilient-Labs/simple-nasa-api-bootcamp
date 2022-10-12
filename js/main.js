be0d911144ff0702b04be68b1baebc3db1bb382e
Susanna Sebagala <susannasebagala@susannas-mbp.mynetworksettings.com>
2022-10-12 17:22:37 -0400

    answer complete

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+// fetch ('https://api.nasa.gov/planetary/apod?api_key=p5ktc0nkovseDxVH0PvyAZ0qd7A8eOzDdG0q0pO1')
+
+document.querySelector('button').addEventListener('click', getPhotos)
+
+function getPhotos() {
+  let date = document.querySelector("input").value;
+  let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${apiKey}`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data.photos[0].img_src);
+      document.querySelector("img").src = data.photos[0].img_src;
+
+      //   complex fetch is fetch inside of a fetch so one runs before the other
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+  
+
+}

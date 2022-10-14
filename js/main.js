7beeca065fc6e69a742f5da47a4c68a6d35db31b
Susanna Sebagala <susannasebagala@Susannas-MacBook-Pro.local>
2022-10-14 01:05:44 -0400

    netifly

--- js/main.js
+++ js/main.js
@@ -6,13 +6,17 @@ document.querySelector('button').addEventListener('click', getPhotos)
 
 function getPhotos() {
   let date = document.querySelector("input").value;
-  let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${apiKey}`;
+  // let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&api_key=${apiKey}`;
 
-  fetch(url)
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=p5ktc0nkovseDxVH0PvyAZ0qd7A8eOzDdG0q0pO1&date=${date}`)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      console.log(data.photos[0].img_src);
-      document.querySelector("img").src = data.photos[0].img_src;
+      console.log(data);
+      // document.querySelector("img").src = data.photos[0].img_src;
+
+      document.querySelector("img").src = data.hdurl;
+      document.querySelector('h2').innerText = data.title;
+      document.querySelector('h3').innerText = data.explanation;
 
       //   complex fetch is fetch inside of a fetch so one runs before the other
     })

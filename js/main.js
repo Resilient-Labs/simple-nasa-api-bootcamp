d71205dbe4bdd57c2492d0e5715163d73033550a
valerythedev <valgonzr@gmail.com>
2023-04-05 14:09:52 -0400

    simple NASA

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@
 // the NASA picture of the day from that date! https://api.nasa.gov/
 console.log("hello")
 document.querySelector('button').addEventListener('click', whatHappenedThatDay)
+const iframe = document.querySelector('iframe')
+const image = document.querySelector('img')
+
 function whatHappenedThatDay() {
   let whatDay = document.querySelector('input').value
   const url = (`https://api.nasa.gov/planetary/apod?api_key=bGCyq1wm8NKvZ45QwgeJR95DstfhSWscgrOKuMbg&date=${whatDay}`)
-console.log('hello')
+  // console.log('hello')
   fetch(url)
     .then(res => res.json())
     .then(data => {
@@ -13,12 +16,12 @@ console.log('hello')
       document.querySelector('h2').innerText = data.title
 
       if (data.media_type === 'image') {
-        document.querySelector('img').src = data.url
+        image.src = data.url
         image.classList.remove('hide')
         iframe.classList.add('hide')
       } else if (data.media_type === 'video') {
         iframe.classList.remove('hide')
-        document.querySelector('iframe').src = data.url
+        iframe.src = data.url
         image.classList.add('hide')
       }
       document.querySelector('span').innerText = data.date

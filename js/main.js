90f326ef8d6071789f7452320fdb14b48196e0de
Joyce Zhang <jzzhang.dev@gmail.com>
2023-04-09 19:26:14 -0400

    First Commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
+
+let image = document.querySelector('img')
+let iframe = document.querySelector('iframe')
+
+function getPic(){
+    let selection = document.querySelector('input').value
+   let url= `https://api.nasa.gov/planetary/apod?api_key=oegTJJPhevUO3prLfAdaLbU14DPOCVNigQWo77jX&date=${selection}`
+
+   fetch(url)
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+console.log(data)
+document.querySelector('h2').innerText= data.title;
+document.querySelector('h3').innerText= data.explanation
+
+if(data.media_type === "image"){
+    document.querySelector('img').src = data.url
+    image.classList.remove("hidden");
+    iframe.classList.add("hidden");
+} else if(data.media_type === "video"){
+    document.querySelector('iframe').src = data.url
+    image.classList.add("hidden");
+    iframe.classList.remove("hidden");
+} else{
+    console.log("unknown media type", data.media_type)
+}
+
+  
+
+})
+.catch(err => {
+console.log(`error ${err}`)
+});
+
+}
+
+
+document.querySelector('button').addEventListener( "click", getPic)
